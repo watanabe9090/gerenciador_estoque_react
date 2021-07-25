d1d88c5521fd6d42e371e35dfd896ff44f35e5a7
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-07-25 14:07:22 -0300

    Constantes utilizadas em todo o projeto

---
+++ src/util/genderOptions.js
@@ -0,0 +1,7 @@
+const genderOptions = [
+  { text: 'Masculino', value: 'm' },
+  { text: 'Feminino', value: 'f' },
+  { text: 'Outro', value: 'o' },
+];
+
+export default genderOptions;
---
+++ src/util/sexos.js
@@ -0,0 +1,10 @@
+const sexos = {
+  "f": "Feminino",
+  "F": "Feminino",
+  "m": "Masculino",
+  "M": "Masculino",
+  "o": "Outros",
+  "O": "Outros"
+}
+
+module.exports = sexos;
