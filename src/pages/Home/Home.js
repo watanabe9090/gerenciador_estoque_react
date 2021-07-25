051a996c6344cad213da3eea62b73c85ae776fb4
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-07-25 14:11:47 -0300

    Pagina de Home provisória

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class HomePage extends React.Component {
+	
+	
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Home Page</h1>
+			</React.Fragment>
+			);
+		};
+	}
