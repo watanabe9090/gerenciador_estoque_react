d0aefd44212ac28feb9ba0c6266901b19bbd48a4
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-07-26 15:33:34 -0300

    Adição de modal de deleção

--- src/components/Listagem/ListagemBody/ListagemBodyOpcoes/ListagemBodyOpcoes.js
+++ src/components/Listagem/ListagemBody/ListagemBodyOpcoes/ListagemBodyOpcoes.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import {Button, Popup} from 'semantic-ui-react';
+import React, {useState} from 'react';
+import {Button, Popup, Modal, Header, Icon} from 'semantic-ui-react';
 
 const Opcoes = (props) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <td>
       <Popup 
@@ -12,6 +14,43 @@ const Opcoes = (props) => {
         content='Detalhes' 
         trigger={<Button inverted color='green' icon='magnify' href={props.path + '/detalhes/' + props.id} />} 
       />
+      <Popup 
+        content='Inativar Registro' 
+        trigger={<Button inverted color='red' icon='delete' onClick={() => setOpen(true)}/>} 
+      />
+      
+      <Modal 
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+      >
+        <Header icon>
+          <Icon name='archive' />
+          Deseja Realmente Inativar esse registro?
+        </Header>
+        <Modal.Content>
+          <p>
+            Após a inativação de um registro, ele não ficará mais visível nas listagens e consultas,
+            entretanto poderá ser visto ainda quando alguma outra entidade tiver relação com este
+          </p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button 
+            inverted
+            content='Não'
+            color='red' 
+            icon='remove'
+            onClick={() => setOpen(false)}
+          />
+          <Button 
+            inverted
+            content='Sim'
+            color='green'
+            icon='checkmark'
+            onClick={() => setOpen(false)}
+          />
+        </Modal.Actions>
+      </Modal>
     </td>
   );
 }
