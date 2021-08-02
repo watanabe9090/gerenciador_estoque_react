937890eb51f8adb503d14a546612f2399143aca2
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-08-02 09:26:23 -0300

    Informações de Input do Fornecedor

---
+++ src/domain_files/Fornecedor/FornecedorInputInfo.js
@@ -0,0 +1,11 @@
+import { InputInfo } from '../../components/InputInfo/InputInfo';
+
+const InputInfoNomeFantasia = <InputInfo label='Nome Fantasia' popup='Designação popular ou titulo da empresa'/>
+const InputInfoRazaoSocial = <InputInfo label='Razão Social' popup='Nome Jurídico da empresa'/>
+const InputInfoCnpj = <InputInfo label='CNPJ' popup='Cadastro Nacional da pessoa Jurídica'/>
+
+export {
+  InputInfoNomeFantasia,
+  InputInfoRazaoSocial,
+  InputInfoCnpj
+};
