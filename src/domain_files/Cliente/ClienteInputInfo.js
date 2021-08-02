63374a285882b8630f583d35b93a37a4ebe3a70d
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-08-02 09:26:06 -0300

    Informações de Input do Cliente

---
+++ src/domain_files/Cliente/ClienteInputInfo.js
@@ -0,0 +1,13 @@
+import { InputInfo } from '../../components/InputInfo/InputInfo';
+
+const InputInfoNome = <InputInfo label='Nome' popup='Primeiro nome do Indivíduo'/>
+const InputInfoSobrenome = <InputInfo label='Sobrenome' popup='Sobrenome do Indivíduo'/>
+const InputInfoSexo = <InputInfo label='Sexo' popup='Sexo do Indivíduo'/>
+const InputInfoCpf = <InputInfo label='CPF' popup='Comprovante de pessoa física'/>
+
+export {
+  InputInfoNome,
+  InputInfoSobrenome,
+  InputInfoSexo,
+  InputInfoCpf
+};
