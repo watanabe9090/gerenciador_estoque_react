a1bc9008bde3c66c207412a465e67f1064006dce
Yuri Takae <yuriwatanabe9090@gmail.com>
2021-08-03 11:08:09 -0300

    Ajuste na pagina de NotFound

---
+++ src/pages/NotFound/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NotFound from '../../components/NotFound/NotFound';
+function NotFoundPage() {
+	return(
+		<React.Fragment>
+			<NotFound />
+		</React.Fragment>
+		)
+	}
+	
+	export default NotFoundPage;
