445a5731288f0e9e3ae202c6bacbf9172cd9e97c
渡辺ユリ <yuriwatanabe9090@gmail.com>
2021-10-09 21:42:42 -0300

    Ajuste na exibição de campos vazios no detalhamento do fornecedor

--- src/pages/FornecedorDetalhes/FornecedorDetalhes.js
+++ src/pages/FornecedorDetalhes/FornecedorDetalhes.js
@@ -19,19 +19,19 @@ const FornecedorDetalhes = (props) => {
         cells: [field, value ]
       });
       const tableData = [
-        {field:"ID:" ,value:fornecedor.id},
-        {field:"Nome Fantasia", value:fornecedor.nomeFantasia},
-        {field:"Razão Social", value:fornecedor.razaoSocial},
-        {field:"CNPJ", value:fornecedor.cnpj},
-        {field:"E-mail", value:fornecedor.contato.email},
-        {field:"Telefone Fixo", value:fornecedor.contato.telefoneFixo},
-        {field:"Telefone Celular", value:fornecedor.contato.telefoneCelular},
-        {field:"CEP", value:fornecedor.endereco.cep},
-        {field:"Localidade", value:fornecedor.endereco.localidade},
-        {field:"Bairro", value:fornecedor.endereco.bairro},
-        {field:"Número", value:fornecedor.endereco.numero},
-        {field:"Logradouro", value:fornecedor.endereco.logradouro},
-        {field:"Complemento", value:fornecedor.endereco.complemento}
+        {field:"ID:" ,value:fornecedor.id || ''},
+        {field:"Nome Fantasia", value:fornecedor.nomeFantasia || ''},
+        {field:"Razão Social", value:fornecedor.razaoSocial || ''},
+        {field:"CNPJ", value:fornecedor.cnpj || ''},
+        {field:"E-mail", value:fornecedor.contato.email || ''},
+        {field:"Telefone Fixo", value:fornecedor.contato.telefoneFixo || ''},
+        {field:"Telefone Celular", value:fornecedor.contato.telefoneCelular || ''},
+        {field:"CEP", value:fornecedor.endereco.cep || ''},
+        {field:"Localidade", value:fornecedor.endereco.localidade || ''},
+        {field:"Bairro", value:fornecedor.endereco.bairro || ''},
+        {field:"Número", value:fornecedor.endereco.numero || ''},
+        {field:"Logradouro", value:fornecedor.endereco.logradouro || ''},
+        {field:"Complemento", value:fornecedor.endereco.complemento || ''}
       ];
       return(
         <React.Fragment>
