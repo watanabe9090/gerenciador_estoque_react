a70f2a29e8037ca11a94aa9cb3ae22529d2d503f
渡辺ユリ <yuriwatanabe9090@gmail.com>
2021-10-13 08:47:29 -0300

    Ajuste no label do componente de Endereço

--- src/components/Endereco/EnderecoInputInfo.js
+++ src/components/Endereco/EnderecoInputInfo.js
@@ -1,11 +1,11 @@
 import { InputInfo } from "../InputInfo/InputInfo";
 
-const InputInfoCep = <InputInfo label='CEP' popup='Código de Endereçamento Postal (sem traços ou pontuações), EX:01011100 ' />
-const InputInfoLogradouro = <InputInfo label='Logradouro' popup='Ruas, Avenidas, ...' />
-const InputInfoBairro = <InputInfo label='Bairro' popup='' />
-const InputInfoLocalidade = <InputInfo label='Localidade' popup='Município em que se encontra' />
-const InputInfoNumero = <InputInfo label='Número' popup='Número da Residência' />
-const InputInfoComplemento = <InputInfo label='Complemento' popup='Informações extras para indicar com exatidão a localização correta' />
+const InputInfoCep = <InputInfo label='CEP' popup='Código de Endereçamento Postal (sem traços ou pontuações) Ex:01011100 ' />
+const InputInfoLogradouro = <InputInfo label='Logradouro' popup='Ruas, Avenidas, ... Ex: Rua São Bento' />
+const InputInfoBairro = <InputInfo label='Bairro' popup='Subáreas das cidades e munícipios Ex: Centro' />
+const InputInfoLocalidade = <InputInfo label='Localidade' popup='Município ou cidade em que se encontra Ex: São Paulo' />
+const InputInfoNumero = <InputInfo label='Número' popup='Número da Residência Ex: 912' />
+const InputInfoComplemento = <InputInfo label='Complemento' popup='Informações extras para indicar com exatidão a localização correta, Ex: lado ímpar ...' />
 
 
 
