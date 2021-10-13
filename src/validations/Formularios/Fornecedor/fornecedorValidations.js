504f9c0d973eee4e782ded28fdc0d26f07df6d66
渡辺ユリ <yuriwatanabe9090@gmail.com>
2021-10-13 09:17:06 -0300

    Validações do Fornecedor

--- src/validations/Formularios/Fornecedor/fornecedorValidations.js
+++ src/validations/Formularios/Fornecedor/fornecedorValidations.js
@@ -1,25 +1,32 @@
 import {justIntegers, justLetterAndSpaces } from '../../regexPatterns'; 
 
 function validate(values) {
-  const errors = {initialize:true};
+  const errors = {
+    cnpj: null,
+    nomeFantasia: null,
+    razaoSocial: null,
+    ok: true
+  };
   const {cnpj, razaoSocial, nomeFantasia} = values;
 
   if(nomeFantasia.length < 1) {
     errors.nomeFantasia = "O Nome Fantasia não pode estar em branco";
+    errors.ok = false;
   }
   if (!justLetterAndSpaces.test(razaoSocial)) {
     errors.razaoSocial = "Razão Social deve conter apenas letras";
+    errors.ok = false;
   }
   if(!justIntegers.test(cnpj)) {
     errors.cnpj = "CNPJ deve contar apenas números";
+    errors.ok = false;
   }
   if (cnpj.length != 14) {
     errors.cnpj = "Número de Caracters do CNPJ inválido Inválido";
+    errors.ok = false;
   }
-
   return errors;
 
-  // console.log(Object.keys(values)); 
 }
 
 export {validate};
